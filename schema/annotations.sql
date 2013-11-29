0bab123eb8acf03a6d9169855c75db13ae1ef78d
Stefan Khan-Kernahan <skhanker@gmail.com>
2013-11-29 15:28:19 -0800

    testing commit from source-tree

---
+++ schema/annotations.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `Annotations` (
+			`handle`  int NULL ,
+			`pageindex`  int NULL ,
+			`x-coord`  int NULL ,
+			`y-coord`  int NULL ,
+			`width`  int NULL ,
+			`height`  int NULL ,
+			`content`  text NULL 
+			)
+		;
